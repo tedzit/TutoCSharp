30042891bb734ffd88e72e904c96e79b7caef583
Tedzit <morell.ted.902@gmail.com>
2020-02-21 20:06:35 -0600

    Version 1 Tutoriel CSharp plus ou moins

---
+++ PlusOuMoins/PlusOuMoins/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusOuMoins
+{
+    class Program
+    {
+        class nombreInconnu
+        {
+            private int m_nonbreInconnu;
+
+            public nombreInconnu()
+            {
+                System.Random rand = new System.Random();
+                m_nonbreInconnu = rand.Next(100);
+            }
+
+            public int Nombre
+            {
+                get { return m_nonbreInconnu; }
+            }
+  
+        }
+
+        static void Main(string[] args)
+        {
+            nombreInconnu nombreInconnu=new nombreInconnu() ;
+            Console.WriteLine("Bienvennue au super jeu de marde !");
+            Console.WriteLine("Entrez un nombre entre 0 et 100");
+            string str = Console.ReadLine();
+            int guess = 0;
+            int.TryParse(str, out guess);
+            int nombre = nombreInconnu.Nombre;
+            while (guess != nombre)
+            {
+                if (guess < nombre)
+                {
+                    Console.WriteLine("C'est plus !");
+                    Console.WriteLine("nouvelle tentative!");
+                    string str2 = Console.ReadLine();
+                    int.TryParse(str2, out guess);
+                }
+                else if (guess > nombre)
+                {
+                    Console.WriteLine("C'est moins !");
+                    Console.WriteLine("nouvelle tentative!");
+                    string str2 = Console.ReadLine();
+                    int.TryParse(str2, out guess);
+                }
+                else
+                {
+                Console.WriteLine("C'est bon !");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
