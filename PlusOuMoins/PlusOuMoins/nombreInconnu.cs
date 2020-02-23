219a777e4a533cbf7325f7fa36af9f400f5e730b
Tedzit <morell.ted.902@gmail.com>
2020-02-22 20:25:44 -0500

    Séparation classe et programme principal

---
+++ PlusOuMoins/PlusOuMoins/nombreInconnu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusOuMoins
+{
+    public class nombreInconnu
+    {
+        public nombreInconnu()
+        {
+            System.Random rand = new System.Random();
+            m_nonbreInconnu = rand.Next(100);
+        }
+
+        public int Nombre
+        {
+            get { return m_nonbreInconnu; }
+        }
+
+        private int m_nonbreInconnu;
+    }
+}
